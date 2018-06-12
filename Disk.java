0779a28ada6ef099cc2586a90a3c2b5d8b80d80c
MinGleP <noona96@naver.com>
2018-06-12 19:55:43 +0900

    Unmount,formatDisk,Console

--- Disk.java
+++ Disk.java
@@ -1,9 +1,12 @@
-//package TermProject;
+package TermProject;
 
-import net.dv8tion.jda.core.EmbedBuilder;
-
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.Vector;
 
 /*
@@ -13,30 +16,180 @@ import java.util.Vector;
 4. 문현균
 5. 손진우
 6. 유승화
-7. 유승화R
+7. 유승화
 8. 손진우
 9. 이인홍
 10. 이인홍
  */
+
+class Console{
+    private SuperBlock sb;
+    private int select;
+    private int num;
+    private int size1,size2;
+    private int count;
+    private BufferedReader in =  new BufferedReader(new InputStreamReader(System.in));
+
+    Console(SuperBlock sb){
+        this.sb = sb;
+        select = -1;
+        count = 0;
+    }
+    public void start(){
+        while(select != 0){
+            System.out.println("File System Management\n"
+                    + "0.  Quit\t"
+                    + "1.  Make disk\t"
+                    + "2.  Format disk\t"
+                    + "3.  Umount disk\t"
+                    + "4.  Empty space in disk\t"
+                    + "6.  Read file\t"
+                    + "7.  Write file\t"
+                    + "8.  Create file\t"
+                    + "9.  Read file by parallel\t"
+                    + "9.  Delete file\t"
+                    + "10. Defragment");
+            try{
+                System.out.print("Choose number: ");
+                select = Integer.parseInt(in.readLine());
+                if(select > 10 || select < 0){
+                    System.out.println("Out of boundary");
+                    continue;
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            switch(select){
+                case 0: System.out.println("System 종료"); break; // 종료
+
+                case 1: //초기화
+                    System.out.println("Decide disk size(two integer): ");
+                    try{
+                        size1 = Integer.parseInt(in.readLine());
+                        size2 = Integer.parseInt(in.readLine());
+                        if(size1 <= 0 || size2 <= 0){
+                            System.out.println("size must be bigger than 0");
+                            break;
+                        }
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                    sb.diskVector.add(new Disk(size1,size2));
+                    System.out.println("Disk is added successfully\n");
+                    break;
+
+                case 2: //format disk
+                    chooseDisk().formatDisk();
+                    System.out.println("Disk got formated successfully");
+                    break;
+                case 3: //unmount disk
+                    sb.unmountDisk(chooseDisk());
+                    System.out.println("Disk got unmounted successfully");
+                    break;
+                case 4: //엠티스페이스
+                    System.out.println("Empty space in the disk: " + chooseDisk().getFreespace());
+                    break;
+                case 5: //리드
+                    chooseDisk();
+                    break;
+                case 6: //라이트
+                    chooseDisk();
+                    break;
+                case 7: //크리에이트
+                    chooseDisk();
+                    break;
+                case 8: //페러럴 리드
+                    chooseDisk();
+                    break;
+                case 9: // 딜리트
+                    chooseDisk();
+                    break;
+                case 10: //디프레그먼트
+                    chooseDisk();
+                    break;
+            }
+        }
+    }
+    private Disk chooseDisk(){
+        System.out.println("Choose a disk");
+        for(int i = 0; i< sb.diskVector.size(); i++){
+            System.out.print(i+ ": " + sb.diskVector.elementAt(i) + " ");
+        }
+        System.out.println();
+        try{
+            System.out.print("Choose number: ");
+            num = Integer.parseInt(in.readLine());
+            if(num >= sb.diskVector.size() || num < 0){
+                System.out.println("Wrong number");
+                return null;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return sb.diskVector.elementAt(num);
+    }
+}
+
+//전역변수 처럼 사용하기 위함.
+class Global {
+    public static int diskID = 0;
+    public static int iNodeID = 0;
+    public static Vector<INODE> iNodeVector = new Vector<>();
+    public static INODE getINODE(Integer id) {
+        for (int i = 0; i < iNodeVector.size(); i++) {
+            if (Integer.compare(iNodeVector.get(i).id, id) == 0)
+                return iNodeVector.get(i);
+        }
+        return null;
+    }
+}
+
+class INODE {
+    public final Integer id;
+    public Integer diskID;      //default null
+    public String owner;        //default null
+    public int startPoint;      //default -1
+    public int size;            //default 0
+    public boolean readOnly;    //default false
+
+    INODE() {
+        id = new Integer(Global.iNodeID++);
+        startPoint = -1;
+        Global.iNodeVector.add(this);
+    }
+}
 
 class SuperBlock {
     Vector<Disk> diskVector;
     SuperBlock() {
         diskVector = new Vector<>();
     }
+    public Disk getDisk(Integer id) {
+        for (int i = 0; i < diskVector.size(); i++) {
+            if (Integer.compare(diskVector.get(i).id, id) == 0)
+                return diskVector.get(i);
+        }
+        return null;
+    }
     //3. unmount disk
     void unmountDisk(Disk disk){
         diskVector.remove(disk);
+
+        for(int tmp :disk.iNodeIDVector){
+            Global.iNodeVector.remove(Global.getINODE(tmp));
+        }
     }
 }
-
 public class Disk {
-    HashMap<Integer, Integer> fileMap;  //index, size
+    public final Integer id;
+    Vector<Integer> iNodeIDVector;
     int[][] blocks;
 
     //1. Initialize
     Disk(int numBlocks, int blockSize) {
-        fileMap = new HashMap<>();
+        id = new Integer(Global.diskID++);
+        iNodeIDVector = new Vector<>();
         blocks = new int[numBlocks][blockSize];
     }
 
@@ -47,40 +200,32 @@ public class Disk {
     int getBlockSize() {
         return getNumBlocks() > 0 ? blocks[0].length : 0;
     }
-
-    //2. format disk
-    void  formatDist(){
-        fileMap.clear();
+    // 2. format disk
+    void formatDisk(){
+//        if(!vec.contains(inodeId)){
+//            System.out.println("There is no requested file");
+//            return;
+//        }
+//        if(Global.iNodeHashMap.get(inodeId).readOnly){
+//            System.out.println("You don't have permission");
+//            return;
+//        }
+        for(int tmp :iNodeIDVector){
+            Global.iNodeVector.remove(Global.getINODE(tmp));
+        }
+        iNodeIDVector.clear();
     }
 
     //4. Empty space
     int getFreespace() {
-        int total = getNumBlocks()*getBlockSize();
-
-        for(Map.Entry<Integer, Integer> elem : fileMap.entrySet()){
-            total -= elem.getValue().intValue();
-        }
-
-        return total;
+        return getNumBlocks()*getBlockSize()
+                - iNodeIDVector.stream().map(i -> Global.getINODE(i).size).mapToInt(i -> i).sum();
     }
 
     public static void main(String[] args) {
         SuperBlock sb = new SuperBlock();
-        Disk disk1 = new Disk(3, 4);
-        Disk disk2 = new Disk(12,24);
-        sb.diskVector.add(disk1);
-        sb.diskVector.add(disk2);
 
-        sb.unmountDisk(disk1);
-        System.out.println(sb.diskVector.size());
-        int tmp  = 1;
-        while(1){
-            System.out.println("FileSystem Management");
-            switch(tmp){
-                case 1:
-                case 2:
-                case 3:
-            }
-        }
+        Console c = new Console(sb);
+        c.start();
     }
 }
